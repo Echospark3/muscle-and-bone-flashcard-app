bec8efa5b79a1a4851094cf9db1b0e9462c21326
Echospark3 <jamesadickson3@gmail.com>
2026-05-10 15:06:21 +0800

    Delete pthon school project directory

--- pthon school project/anatomytype.py
+++
@@ -1,4 +0,0 @@
-class AnatomyPart:
-   def __init__(self, anatomyType, name, location, movement, definition, boneType):
-       self.anatomyType = anatomyType
-       self.name = name
--- pthon school project/question.py
+++
@@ -1,9 +0,0 @@
-class Question: # gets question info and organises it
-   def __init__(self, questionType, questionText, optionA, optionB, optionC, optionD, correctAnswer):
-       self.questionType = questionType
-       self.questionText = questionText
-       self.optionA = optionA
-       self.optionB = optionB
-       self.optionC = optionC
-       self.optionD = optionD
-       self.correctAnswer = correctAnswer
